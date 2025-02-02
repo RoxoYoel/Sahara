cf216cd16fe84f1098084550c3606f5ccf057fd0
Gael Martínez Pais <158576629+Fortgalex@users.noreply.github.com>
2025-02-02 21:26:14 +0100

    er

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,18 +3,49 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject RedScreen;
+    public GameObject YellowScreen;
+    public GameObject GreenScreen;
     void Start()
     {
-        
+        RedScreen.SetActive(false);
     }
 
     void Update()
     {
-        
+        if (GreenScreen.activeSelf)
+        {
+            RedScreen.SetActive(false);
+            YellowScreen.SetActive(false);
+        }
+        else if (YellowScreen.activeSelf)
+        {
+            GreenScreen.SetActive(false);
+            RedScreen.SetActive(false);
+        }
+        else if (RedScreen.activeSelf)
+        {
+            GreenScreen.SetActive(false);
+            YellowScreen.SetActive(false);
+        }
+       
     }
 
     void LoadSceneCamara()
     {
         SceneManager.LoadScene("Camera");
+    }
+
+    void CameraYellow()
+    {   
+        YellowScreen.SetActive(true);    
+    } 
+    void CameraGreen()
+    {   
+        GreenScreen.SetActive(true);    
+    }
+    void CameraRed()
+    {  
+        RedScreen.SetActive(true);    
     }
 }
