67cd9e7164a814ed0b5b98c80ae8f14ec75b61ea
Gael Martínez Pais <158576629+Fortgalex@users.noreply.github.com>
2025-02-02 18:02:11 +0100

    tr

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void LoadSceneCamara()
+    {
+        SceneManager.LoadScene("Cámara");
+    }
+}
