f1437f8507f41ada7a80e012f59fc1984ab94ecf
RoxoYoel <yoelvazenero@gmail.com>
2025-02-04 23:59:43 +0100

    movimiento

--- Assets/Scripts/RPG_Move_v2.cs
+++ Assets/Scripts/RPG_Move_v2.cs
@@ -7,17 +7,32 @@ public class RPG_Move_v2 : MonoBehaviour
     private Vector2 movement;
     private Vector2 lastDirection = Vector2.down; // Dirección inicial mirando hacia abajo
     public Animator anim;
+    SpriteRenderer sp;
 
     void Start()
     {
-        if (rb == null)
-            rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+
+        if (movement.x < 0)
+        {
+            sp.flipX = true;
+        }
+
+        if (movement.x > 0)
+        {
+            sp.flipX = false;
+        }
 
         // Si el personaje se mueve, actualizamos la última dirección
         if (movement != Vector2.zero)
@@ -27,9 +42,8 @@ public class RPG_Move_v2 : MonoBehaviour
 
         if (anim != null)
         {
-            anim.SetFloat("Horizontal", lastDirection.x);
-            anim.SetFloat("Vertical", lastDirection.y);
-            anim.SetFloat("Speed", movement.sqrMagnitude);
+            anim.SetFloat("HorizontalIdle", lastDirection.x);
+            anim.SetFloat("VerticalIdle", lastDirection.y);
         }
     }
 
