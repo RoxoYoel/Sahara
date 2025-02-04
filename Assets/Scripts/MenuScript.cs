78eba30ead781f966a17dca9a3daeb41ed183b90
whisteblower <nachogarciabattista21@gmail.com>
2025-02-04 19:39:19 +0100

    pause menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject pauseMenuCanvas;
+
+    private bool isPaused = false;
     void Start()
     {
-        
+        pauseMenuCanvas.SetActive(false);
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
+    public void PauseGame()
+    {
+        pauseMenuCanvas.SetActive(true);
+        Time.timeScale = 0f; // Detiene el tiempo del juego
+        isPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuCanvas.SetActive(false);
+        Time.timeScale = 1f; // Restaura el tiempo del juego
+        isPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
