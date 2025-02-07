c81b2b484e44170b8740f4034b0f605ffef0f59b
whisteblower <nachogarciabattista21@gmail.com>
2025-02-07 17:58:18 +0100

    rth

--- Assets/Scripts/NPCFacePlayer.cs
+++ Assets/Scripts/NPCFacePlayer.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
-public class NPCFacePlayer : MonoBehaviour
+public class NPCFacing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Animator npcAnimator;
+    public Transform playerTransform;
+
+    // Call this method when the dialogue starts
+    public void FacePlayer()
     {
-        
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+
+        // Determine facing direction based on player's position
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+                SetFacing("Right");
+            else
+                SetFacing("Left");
+        }
+        else
+        {
+            if (direction.y > 0)
+                SetFacing("Front");
+            else
+                SetFacing("Back");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetFacing(string direction)
     {
-        
+        npcAnimator.Play($"Idle_{direction}");
     }
 }
